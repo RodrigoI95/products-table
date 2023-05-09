3e6bab6d82247b81766a64f4639fbc506cbcb94e
RodrigoI95 <rodrigo.igu95@gmail.com>
2023-05-08 22:07:26 -0500

    Capitalize columns

--- src/components/ProductsTable/ProductsTable.tsx
+++ src/components/ProductsTable/ProductsTable.tsx
@@ -1,20 +1,25 @@
 import { ProductsTableTypes } from "./types";
 
 const ProductsTable = ({ columns }: ProductsTableTypes) => {
-  const renderColumns = () => {
-    return columns.map((column: string) => {
-      return <th key={column}>{column}</th>;
-    });
-  };
 
-  return (
-    <table>
-      <thead>
-        <tr>{renderColumns()}</tr>
-      </thead>
-      <tbody></tbody>
-    </table>
-  );
+    const capitalizeString = (str: string) => {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+
+    const renderColumns = () => {
+      return columns.map((column: string) => {
+        return <th key={column}>{capitalizeString(column)}</th>;
+      });
+    };
+
+    return (
+      <table>
+        <thead>
+          <tr>{renderColumns()}</tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    );
 };
 
 export default ProductsTable;
