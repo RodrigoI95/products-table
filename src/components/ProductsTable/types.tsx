9ac5eb90d00b0bd14ff13ad4e4518f6b3f3a56a3
RodrigoI95 <rodrigo.igu95@gmail.com>
2023-05-08 22:46:25 -0500

    Updated table types

--- src/components/ProductsTable/types.tsx
+++ src/components/ProductsTable/types.tsx
@@ -1,3 +1,13 @@
+export type ProductData = {
+  code: string;
+  position: number;
+  quantity: number;
+  image: string;
+  price: number;
+  description: string;
+};
+
 export type ProductsTableTypes = {
   columns: string[];
+  rowsData: ProductData[];
 };
